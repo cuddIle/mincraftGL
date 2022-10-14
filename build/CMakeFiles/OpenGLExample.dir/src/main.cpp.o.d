dc82030ce2bf3e2932b0951dad6b86ae9498a325
Yout name <you@example.com>
2022-10-14 09:39:42 +0300

    worgink with no debug

---
+++ build/CMakeFiles/OpenGLExample.dir/src/main.cpp.o.d
@@ -0,0 +1,15 @@
+CMakeFiles/OpenGLExample.dir/src/main.cpp.o: \
+ /home/cuddle/projects/opengl/src/main.cpp /usr/include/stdc-predef.h \
+ /home/cuddle/projects/opengl/include/glad/glad.h \
+ /usr/include/KHR/khrplatform.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /home/cuddle/projects/opengl/include/glfw/glfw3.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include/stddef.h
